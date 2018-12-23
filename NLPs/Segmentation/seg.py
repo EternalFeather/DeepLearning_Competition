f8c164bb3ddb17ade36e7c387416ffbe85fa24eb
EternalFeather <czj259400@gmail.com>
2018-12-23 17:58:04 +0800

    update tnt segmentation algorithm

--- NLPs/Segmentation/seg.py
+++ NLPs/Segmentation/seg.py
@@ -1,4 +1,42 @@
+from NLPs.Segmentation.tnt import Tnt
+from NLPs.Segmentation.cbgm import Cbgm
+
 
 class Seg(object):
-	def __init__(self):
-		pass
+	def __init__(self, name='cbgm'):
+		if name == 'tnt':
+			self.segger = Tnt()
+		else:
+			self.segger = Cbgm()
+
+	def train(self, fname):
+		f = open(fname, 'r', encoding='utf-8')
+		data = []
+		for line in f:
+			line = line.strip()
+			if not line:
+				continue
+			tmp = map(lambda x: x.split('/'), line.split(' '))
+			data.append(tmp)
+		f.close()
+		self.segger.train(data)
+		print('Done!')
+
+	def restore(self):
+		self.segger.restore()
+
+	def cut(self, sentence):
+		ret = self.segger.cut(sentence)
+		tmp = ""
+		for i in ret:
+			if i[1] == 'e':
+				yield tmp + i[0]
+				tmp = ""
+			elif i[1] == 'b' or i[1] == 's':
+				if tmp:
+					yield tmp
+				tmp = i[0]
+			else:
+				tmp += i[0]
+		if tmp:
+			yield tmp
--- NLPs/__init__.py
+++ NLPs/__init__.py
@@ -1,1 +1,18 @@
 from .Segmentation.seg import Seg
+
+
+class NLPTools(object):
+	def __init__(self, cut_fn, cut_type):
+		# 断词
+		self.cut_fn = cut_fn
+		self.seg = Seg(cut_type)
+
+	def cutword(self, doc, train=False):
+		if not train:
+			self.seg.restore()
+			return [word for word in self.seg.cut(doc)]
+		else:
+			print("MSG : Start training segmentation service ...")
+			self.seg.train(self.cut_fn)
+
+
